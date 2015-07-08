c61523581df1856a7b838f8fe192cd5a55976ff4
blacroix <benjlacroix@gmail.com>
2015-07-08 18:36:52 +0200

    Change theme to pink and orange

--- app/app.js
+++ app/app.js
@@ -3,4 +3,9 @@
 angular.module('app', ['ngMaterial', 'ngRoute', 'ngResource'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', {controller: 'LoginCtrl', templateUrl: 'app/login/tmpl.html'})
+    }])
+    .config(['$mdThemingProvider', function ($mdThemingProvider) {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('pink')
+            .accentPalette('orange');
     }]);
